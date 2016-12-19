a38c5187e69674ed616b99744508c291f2297293
SuperDHQ <dhq0317@126.com>
2016-12-19 15:48:49 +0800

    测试

---
+++ app/src/main/java/com/wxb/jianbao11/activity/PublishedActivity.java
@@ -0,0 +1,15 @@
+package com.wxb.jianbao11.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Administrator on 2016/12/19.
+ */
+
+public class PublishedActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
