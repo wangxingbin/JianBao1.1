623048daa9df1f565d95e7701c6b889174a07dcc
ti2016 <m15035023129@163.com>
2016-12-23 13:42:15 +0800

    lllll

--- app/src/main/java/com/wxb/jianbao11/activity/SPXQActivity.java
+++ app/src/main/java/com/wxb/jianbao11/activity/SPXQActivity.java
@@ -5,14 +5,17 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -43,7 +46,8 @@ public class SPXQActivity extends Activity {
     private int vpheight;
     private ImageView wpgzFollow;
     private String id;
-    private TextView wpfbTime,wpPrice,wpTitle,wpDescription,wpMobile,wpQQ,wpWechat,wpEmail,tv_follownumber,tv_person;
+    private TextView wpfbTime,wpPrice,wpTitle,wpDescription,wpMobile,wpQQ,
+            wpWechat,wpEmail,tv_follownumber,tv_person,wuping_pel;
     private SimpleDraweeView draweeView;
     private ArrayList<ImageView> dian;
     private SharedPreferences sharedPreferences;
@@ -64,6 +68,13 @@ public class SPXQActivity extends Activity {
     private Button bt_xiajia;
     private Button bt_xiangyao;
     private TextView bar_tv_name;
+    private String wecat;
+    private String email;
+    private String qq;
+    private String mobile;
+    private String contact;
+    private PopupWindow popupWindow;
+    private String head;
 
 
     @Override
@@ -121,6 +132,95 @@ public class SPXQActivity extends Activity {
 
             }
         });
+        //想要的按钮
+        bt_xiangyao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupWindow();
+
+            }
+        });
+
+    }
+
+
+
+    private void showPopupWindow() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(SPXQActivity.this);
+        // 一个自定义的布局，作为显示的内容
+        View v = LayoutInflater.from(SPXQActivity.this).inflate(
+                R.layout.pop, null);
+
+        SimpleDraweeView  sdv = (SimpleDraweeView) v.findViewById(R.id.sdv);
+        wuping_pel = (TextView) v.findViewById(R.id.wuping_pel);
+        wpMobile = (TextView) v.findViewById(R.id.wuping_phone);
+        wpQQ = (TextView) v.findViewById(R.id.wuping_qq);
+        TextView wpQQ1 = (TextView) v.findViewById(R.id.wuping_qq1);
+        wpWechat = (TextView) v.findViewById(R.id.wuping_weixin);
+        TextView wpWechat1 = (TextView) v.findViewById(R.id.wuping_weixin1);
+        wpEmail = (TextView) v.findViewById(R.id.wuping_email);
+        TextView wpEmail1 = (TextView) v.findViewById(R.id.wuping_email1);
+
+
+        Uri uri = Uri.parse("http://192.168.4.188/Goods/uploads/"+ head);
+        sdv.setImageURI(uri);
+
+        wuping_pel.setText(contact);
+        if (TextUtils.isEmpty(mobile)) {
+            wpMobile.setText("");
+            wpMobile.setVisibility(View.GONE);
+        } else {
+            wpMobile.setVisibility(View.VISIBLE);
+            wpMobile.setText(mobile);
+        }
+        wpMobile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent=new Intent();
+                intent.setAction(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:"+ mobile));
+                startActivity(intent);
+
+
+            }
+        });
+
+        if (TextUtils.isEmpty(qq)) {
+            wpQQ.setText("");
+            wpQQ.setVisibility(View.GONE);
+            wpQQ1.setVisibility(View.GONE);
+        } else {
+            wpQQ.setVisibility(View.VISIBLE);
+            wpQQ1.setVisibility(View.VISIBLE);
+            wpQQ.setText(qq);
+        }
+        if (TextUtils.isEmpty(email)) {
+            wpEmail.setText("");
+            wpEmail1.setVisibility(View.GONE);
+            wpEmail.setVisibility(View.GONE);
+            }
+        else {
+            wpEmail.setVisibility(View.VISIBLE);
+            wpEmail1.setVisibility(View.VISIBLE);
+            wpEmail.setText(email);
+        }
+        if (TextUtils.isEmpty(wecat)) {
+            wpWechat.setText("");
+            wpWechat.setVisibility(View.GONE);
+            wpWechat1.setVisibility(View.GONE);
+        } else {
+            wpWechat.setVisibility(View.VISIBLE);
+            wpWechat1.setVisibility(View.VISIBLE);
+            wpWechat.setText(wecat);
+        }
+
+
+
+        builder.setView(v);//添加自定义View
+        builder.create();
+        builder.show();
 
     }
 
@@ -225,11 +325,7 @@ public class SPXQActivity extends Activity {
                 id.wuping_shoucang);
         wpDescription = (TextView) findViewById(R.id.wuping_xiangqing);
         tv_person = (TextView) findViewById(R.id.tv_person);
-        wpMobile = (TextView) findViewById(R.id.wuping_phone);
         iv_head = (SimpleDraweeView) findViewById(R.id.my_image_view);
-        wpQQ = (TextView) findViewById(R.id.wuping_qq);
-        wpWechat = (TextView) findViewById(R.id.wuping_weixin);
-        wpEmail = (TextView) findViewById(R.id.wuping_email);
         ll_gz = (LinearLayout) findViewById(R.id.ll_gz);
         dgsptp_rv = (RecyclerView) findViewById(R.id.dgsptp_rv);
         FullyLinearLayoutManager manager = new FullyLinearLayoutManager(SPXQActivity.this);
@@ -238,6 +334,7 @@ public class SPXQActivity extends Activity {
         dgsptp_rv.setLayoutManager(manager);
         bt_xiajia = (Button) findViewById(R.id.bt_xiajia);
         bt_xiangyao = (Button) findViewById(R.id.bt_xiangyao);
+
 
 
     }
@@ -295,16 +392,16 @@ public class SPXQActivity extends Activity {
         final String time = sp.getData().getIssue_time();
         final String description = sp.getData().getDescription();
         final String price = sp.getData().getPrice();
-        final String mobile = sp.getData().getMobile();
-        final String qq = sp.getData().getQq();
-        final String email = sp.getData().getEmail();
-        final String wecat = sp.getData().getWechat();
-        final String head=sp.getData().getHead();
+        mobile = sp.getData().getMobile();
+        qq = sp.getData().getQq();
+        email = sp.getData().getEmail();
+        wecat = sp.getData().getWechat();
+        head = sp.getData().getHead();
         final int state=sp.getData().getState();
         final boolean owned=sp.getData().isOwned();
         follow = sp.getData().getFollow();
         followed = sp.getData().isFollowed();
-        final String Contact = sp.getData().getContact();
+        contact = sp.getData().getContact();
 
         runOnUiThread(new Runnable() {
             @Override
@@ -315,40 +412,11 @@ public class SPXQActivity extends Activity {
                 wpDescription.setText(description);
                 wpPrice.setText("¥"+price);
                 tv_follownumber.setText("关注人数:"+follow );
-                tv_person.setText(Contact);
+                tv_person.setText(contact);
 
                 Uri uri = Uri.parse("http://192.168.4.188/Goods/uploads/"+ head);
                 iv_head.setImageURI(uri);
 
-                if (TextUtils.isEmpty(mobile)) {
-                    wpMobile.setText("");
-                    wpMobile.setVisibility(View.GONE);
-                } else {
-                    wpMobile.setVisibility(View.VISIBLE);
-                    wpMobile.setText("联系电话：" + sp.getData().getMobile());
-                }
-
-                if (TextUtils.isEmpty(qq)) {
-                    wpQQ.setText("");
-                    wpQQ.setVisibility(View.GONE);
-                } else {
-                    wpQQ.setVisibility(View.VISIBLE);
-                    wpQQ.setText("Q Q：" + sp.getData().getMobile());
-                }
-                if (TextUtils.isEmpty(email)) {
-                    wpEmail.setText("");
-                    wpEmail.setVisibility(View.GONE);
-                } else {
-                    wpEmail.setVisibility(View.GONE);
-                    wpEmail.setText("Email：" + sp.getData().getMobile());
-                }
-                if (TextUtils.isEmpty(wecat)) {
-                    wpWechat.setText("");
-                    wpWechat.setVisibility(View.GONE);
-                } else {
-                    wpWechat.setVisibility(View.VISIBLE);
-                    wpWechat.setText("微 信：" + sp.getData().getMobile());
-                }
                 if (followed) {
                    wpgzFollow.setBackgroundResource(R.mipmap.scd);
                 } else {
