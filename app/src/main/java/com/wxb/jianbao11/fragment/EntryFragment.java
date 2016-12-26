bbe4e92d797f7dab695a23452f0862dcb6756ffa
ZhaoUnruly <android_zzq@163.com>
2016-12-26 08:56:51 +0800

    发布完成  12.26.8：56

--- app/src/main/java/com/wxb/jianbao11/fragment/EntryFragment.java
+++ app/src/main/java/com/wxb/jianbao11/fragment/EntryFragment.java
@@ -8,9 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.wxb.jianbao11.MainActivity;
-import com.wxb.jianbao11.activity.GuidePageActivity;
 import com.wxb.jianbao11.R;
+import com.wxb.jianbao11.activity.GuidePageActivity;
 import com.wxb.jianbao11.activity.Login;
 
 /**
